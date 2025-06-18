f0b73f9056870acc9a11c1f3e2ddf8e5eea0b052
Turtlewords <144561651+Turtlewords@users.noreply.github.com>
2025-06-18 13:25:00 -0500

    add slide menu

--- app/js/index.js
+++ app/js/index.js
@@ -1,17 +1,22 @@
 // Elements
 
 const headMenuBtn = document.querySelector(".head__menu");
-const slideMenu = document.querySelector
+const slideMenu = document.querySelector(".slide-menu");
+const closeMenuBtn = document.querySelector(".slide-menu__close-btn")
 
 // Event Listeners
 
-headMenu.addEventListener("click", () => {
-    showMenu();
-})
+headMenuBtn.addEventListener("click", showMenu)
+
+closeMenuBtn.addEventListener("click", closeMenu)
 
 
 // Functions
 
 function showMenu() {
+    slideMenu.style.width = "100%";
+}
 
+function closeMenu() {
+    slideMenu.style.width = "0";
 }
