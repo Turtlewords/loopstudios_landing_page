19decd0a6cb9b8c08967395312794034eadb156f
turtlewords <144561651+Turtlewords@users.noreply.github.com>
2025-06-18 12:57:53 -0500

    begin slide menu for mobile

---
+++ app/js/index.js
@@ -0,0 +1,17 @@
+// Elements
+
+const headMenuBtn = document.querySelector(".head__menu");
+const slideMenu = document.querySelector
+
+// Event Listeners
+
+headMenu.addEventListener("click", () => {
+    showMenu();
+})
+
+
+// Functions
+
+function showMenu() {
+
+}
