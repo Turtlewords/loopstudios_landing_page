faeb9772844009beb8ca5af3389c0a35e3a5147b
Turtlewords <144561651+Turtlewords@users.noreply.github.com>
2025-06-20 13:12:19 -0500

    slight style tweaks

--- app/js/index.js
+++ app/js/index.js
@@ -4,11 +4,15 @@ const headMenuBtn = document.querySelector(".head__menu");
 const slideMenu = document.querySelector(".slide-menu");
 const closeMenuBtn = document.querySelector(".slide-menu__close-btn")
 
+
+
 // Event Listeners
 
 headMenuBtn.addEventListener("click", showMenu)
 
 closeMenuBtn.addEventListener("click", closeMenu)
+
+
 
 
 // Functions
